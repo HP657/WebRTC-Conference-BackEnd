26b32b76ef0d56337f510e9a0863f669e883c7cd
HP657 <junlee5070@gmail.com>
2025-12-30 02:36:44 +0900

    build: 디펜더시 메니지먼트 플러그인 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-	kotlin("jvm") version "1.9.25" apply false
-	kotlin("plugin.spring") version "1.9.25" apply false
-	id("org.springframework.boot") version "3.5.9" apply false
-	id("io.spring.dependency-management") version "1.1.7" apply false
+    kotlin("jvm") version "1.9.25" apply false
+    kotlin("plugin.spring") version "1.9.25" apply false
+    id("org.springframework.boot") version "3.5.9" apply false
+    id("io.spring.dependency-management") version "1.1.7" apply false
 }
 
 group = "com.hp657"
@@ -10,26 +10,27 @@ version = "0.0.1-SNAPSHOT"
 
 subprojects {
 
-	apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "io.spring.dependency-management")
 
-	repositories {
-		mavenCentral()
-	}
+    repositories {
+        mavenCentral()
+    }
 
-	extensions.configure<JavaPluginExtension> {
-		toolchain {
-			languageVersion.set(JavaLanguageVersion.of(17))
-		}
-	}
+    extensions.configure<JavaPluginExtension> {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(17))
+        }
+    }
 
-	tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-		kotlinOptions {
-			jvmTarget = "17"
-			freeCompilerArgs += "-Xjsr305=strict"
-		}
-	}
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "17"
+            freeCompilerArgs += "-Xjsr305=strict"
+        }
+    }
 
-	tasks.withType<Test> {
-		useJUnitPlatform()
-	}
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
 }
