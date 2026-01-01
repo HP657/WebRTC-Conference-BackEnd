3974d565e3e55f6eff29cdccb32e973dad085fb4
HP657 <junlee5070@gmail.com>
2026-01-01 19:18:23 +0900

    feat: 웹소켓 container 세부 세팅

--- application/src/main/kotlin/com/hp657/webrtcconference/WebSocketConfig.kt
+++ application/src/main/kotlin/com/hp657/webrtcconference/WebSocketConfig.kt
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.socket.config.annotation.EnableWebSocket
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean
+import org.springframework.context.annotation.Bean
 
 @Configuration
 @EnableWebSocket
@@ -14,6 +16,16 @@ class WebSocketConfig(
 
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
         registry.addHandler(signalingHandler, "/signal")
-            .setAllowedOrigins("*") // Allow all origins for simplicity, adjust for production
+            .setAllowedOrigins("*")
+    }
+
+    @Bean
+    fun createWebSocketContainer(): ServletServerContainerFactoryBean {
+        val container = ServletServerContainerFactoryBean()
+        container.setMaxTextMessageBufferSize(64 * 1024) // 64KB
+        container.setMaxBinaryMessageBufferSize(64 * 1024)
+        container.setMaxSessionIdleTimeout(10 * 60 * 1000L) // 10분
+        container.setAsyncSendTimeout(5000L) // 5초
+        return container
     }
 }
